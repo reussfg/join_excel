9b29d7f5aa874c2b573af15ac3175db57c4c517b
reussfg <87047060+reussfg@users.noreply.github.com>
2021-07-22 12:04:23 -0300

    Add files via upload

---
+++ Juntador_Excel.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import openpyxl
+
+# Remember to put all the xlsx files into your project root
+path = os.path.abspath('/Users/gabrielreus/PycharmProjects/Juntador_excel/Feito')
+arquivos = os.listdir(path)  # It will locate your archieves here
+df = pd.DataFrame()
+for file in arquivos:
+    # It will select only xlsx files
+    if file.endswith('.xlsx'):
+        df = df.append(pd.read_excel(file), ignore_index=True)
+
+# It will create your merged excel file
+df.to_excel('Combined_file.xlsx')
